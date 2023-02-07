f8a6c515ed4f6cf204a6f614fa852f259028d88d
hiimmurloc <ac1324ac2@gmail.com>
2023-02-07 15:51:05 +0900

    Orders, OrderItem, Payment DTO 수정

--- src/main/java/com/itwill/hotdog/domain/OrderItem.java
+++ src/main/java/com/itwill/hotdog/domain/OrderItem.java
@@ -1,5 +1,57 @@
 package com.itwill.hotdog.domain;
 
 public class OrderItem {
+	private int oi_no;
+	private int oi_qty;
+	private String o_no;
+	private Product product;
+	
+	public OrderItem() {
+		// TODO Auto-generated constructor stub
+	}
 
+	public OrderItem(int oi_no, int oi_qty, String o_no, Product product) {
+		super();
+		this.oi_no = oi_no;
+		this.oi_qty = oi_qty;
+		this.o_no = o_no;
+		this.product = product;
+	}
+
+	public int getOi_no() {
+		return oi_no;
+	}
+
+	public void setOi_no(int oi_no) {
+		this.oi_no = oi_no;
+	}
+
+	public int getOi_qty() {
+		return oi_qty;
+	}
+
+	public void setOi_qty(int oi_qty) {
+		this.oi_qty = oi_qty;
+	}
+
+	public String getO_no() {
+		return o_no;
+	}
+
+	public void setO_no(String o_no) {
+		this.o_no = o_no;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+
+	@Override
+	public String toString() {
+		return "OrderItem [oi_no=" + oi_no + ", oi_qty=" + oi_qty + ", o_no=" + o_no + ", product=" + product + "]";
+	}
 }
