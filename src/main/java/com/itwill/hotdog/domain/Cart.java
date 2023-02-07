6a8d0db76371367a8f03b9970c00a9d3a241f269
itwill <itwill@DESKTOP-JA4DUC3>
2023-02-07 16:32:39 +0900

    sql, service, repository(Product)

--- src/main/java/com/itwill/hotdog/domain/Cart.java
+++ src/main/java/com/itwill/hotdog/domain/Cart.java
@@ -1,5 +1,5 @@
 package com.itwill.hotdog.domain;
 
-public class Cart {
-
+public class Cart{
+	
 }
--- src/main/java/com/itwill/hotdog/sql/ProductSQL.java
+++ src/main/java/com/itwill/hotdog/sql/ProductSQL.java
@@ -1,5 +1,11 @@
 package com.itwill.hotdog.sql;
 
 public class ProductSQL {
-
+	public final static String PRODUCT_SELECT_ALL=
+			"select * from product";
+	public final static String PRODUCT_SELECT_BY_NO=
+			"select * from product where p_no=?";
+	public final static String PRODUCT_SELECT_BY_CNO=
+			"select * from product where ct_no=?";
+	
 }
