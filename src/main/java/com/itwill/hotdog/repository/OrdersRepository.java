7435ba1bf52f9f6f7694e148d50c5c15db896d1e
hiimmurloc <ac1324ac2@gmail.com>
2023-02-07 19:14:03 +0900

    .

--- src/main/java/com/itwill/hotdog/repository/OrdersRepository.java
+++ src/main/java/com/itwill/hotdog/repository/OrdersRepository.java
@@ -1,5 +1,107 @@
 package com.itwill.hotdog.repository;
 
-public class OrdersRepository {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.util.Properties;
 
+import javax.sql.DataSource;
+
+import org.apache.tomcat.dbcp.dbcp2.BasicDataSource;
+
+import com.itwill.hotdog.domain.Orders;
+import com.itwill.hotdog.sql.OrdersSQL;
+
+public class OrdersRepository {
+	
+	private DataSource dataSource;
+	
+	public OrdersRepository() throws Exception {
+		Properties properties = new Properties();
+		properties.load(getClass().getResourceAsStream("/jdbc.properties"));
+		BasicDataSource basicDataSource = new BasicDataSource();
+		basicDataSource.setDriverClassName(properties.getProperty("driverClass"));
+		basicDataSource.setUrl(properties.getProperty("url"));
+		basicDataSource.setUsername(properties.getProperty("username"));
+		basicDataSource.setPassword(properties.getProperty("password"));
+		dataSource = basicDataSource;
+	}
+	
+	/*
+	 * 주문 전체삭제
+	 */
+	public int deleteAll(String sUserId) throws Exception {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		int rowCount = 0;
+		try {
+			con = dataSource.getConnection();
+			pstmt = con.prepareStatement(OrdersSQL.ORDERS_DELETE_BY_U_ID);
+			pstmt.setString(1, sUserId);
+			rowCount = pstmt.executeUpdate();
+		} finally {
+			if(pstmt!=null) pstmt.close();
+			if(con!=null) con.close();
+		}
+		
+		return rowCount;
+	}
+	
+	/*
+	 * 주문 1건 삭제 (선택삭제)
+	 */
+	public int delete(int o_no) throws Exception {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		int rowCount = 0;
+		try {
+			con = dataSource.getConnection();
+			pstmt = con.prepareStatement(OrdersSQL.ORDERS_DELETE_BY_O_NO);
+			pstmt.setInt(1, o_no);
+			rowCount = pstmt.executeUpdate();
+		} finally {
+			if(pstmt!=null) pstmt.close();
+			if(con!=null) con.close();
+		}
+		
+		return rowCount;
+	}
+	
+	/*
+	 * 주문 생성
+	 */
+	public int insert(Orders orders) throws Exception {
+		Connection con = null;
+		PreparedStatement pstmt1 = null;
+		PreparedStatement pstmt2 = null;
+		try {
+			con = dataSource.getConnection();
+			con.setAutoCommit(false);//트랜잭션 시작.
+			pstmt1 = con.prepareStatement(OrdersSQL.ORDERS_INSERT);
+			pstmt1.setInt(1, orders.getO_totalPrice());
+			pstmt1.setInt(2, orders.getO_usedPoint());
+			pstmt1.setInt(3, orders.getPayment().getPm_no());
+			//pstmt1.setString(4, orders.getUserInfo().get);
+		} catch (Exception e) {
+			// TODO: handle exception
+		} finally {
+			
+		}
+		
+		
+		
+		
+		return 0;
+	}
+	
+	/*
+	 * 주문 전체검색 (특정 사용자)
+	 */
+	
+	/*
+	 * 주문 상세보기 전체검색 (특정 사용자) - orders, order_item, product, payment 테이블 JOIN
+	 */
+	
+	/*
+	 * 주문 상세보기 1개 검색 (특정 사용자) - orders, order_item, product, payment 테이블 JOIN
+	 */
 }
--- src/main/java/com/itwill/hotdog/sql/OrdersSQL.java
+++ src/main/java/com/itwill/hotdog/sql/OrdersSQL.java
@@ -1,5 +1,8 @@
 package com.itwill.hotdog.sql;
 
 public class OrdersSQL {
-
+	public static final String ORDERS_DELETE_BY_U_ID = "delete from orders where u_id=?";
+	public static final String ORDERS_DELETE_BY_O_NO = "delete from orders where o_no=?";
+	public static final String ORDERS_INSERT = "insert into orders(o_no, o_date, o_totalprice, o_usedpoint, pm_no, u_id) values (ORDERS_O_NO_SEQ.nextval, sysdate, ?, ?, ?, ?)";
+	public static final String ORDERITEM_INSERT = "insert into order_item(oi_no, oi_qty, o_no, p_no) values(ORDER_ITEM_OI_NO_SEQ.nextval, ?, ORDERS_O_NO_SEQ.currval, ?)";
 }
