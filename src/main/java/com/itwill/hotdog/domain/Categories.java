c3ee3d4d220a123d2ac6fb806621fb25f90a0252
김민영 <117143516+ssssoooop@users.noreply.github.com>
2023-02-07 15:28:36 +0900

    카테고리

--- src/main/java/com/itwill/hotdog/domain/Categories.java
+++ src/main/java/com/itwill/hotdog/domain/Categories.java
@@ -1,5 +1,36 @@
 package com.itwill.hotdog.domain;
 
 public class Categories {
-
+	private int ct_no;
+	private String ct_name;
+	private String ct_img;
+	public int getCt_no() {
+		return ct_no;
+	}
+	public void setCt_no(int ct_no) {
+		this.ct_no = ct_no;
+	}
+	public String getCt_name() {
+		return ct_name;
+	}
+	public void setCt_name(String ct_name) {
+		this.ct_name = ct_name;
+	}
+	public String getCt_img() {
+		return ct_img;
+	}
+	public void setCt_img(String ct_img) {
+		this.ct_img = ct_img;
+	}
+	public Categories(int ct_no, String ct_name, String ct_img) {
+		super();
+		this.ct_no = ct_no;
+		this.ct_name = ct_name;
+		this.ct_img = ct_img;
+	}
+	@Override
+	public String toString() {
+		return "Categories [ct_no=" + ct_no + ", ct_name=" + ct_name + ", ct_img=" + ct_img + "]";
+	}
+	
 }
