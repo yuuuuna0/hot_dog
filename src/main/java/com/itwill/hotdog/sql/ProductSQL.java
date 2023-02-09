e22a21ec1102a09464e7bb4065ef8c1836de3a8a
김민영 <117143516+ssssoooop@users.noreply.github.com>
2023-02-09 19:44:52 +0900

    Update ProductSQL.java

--- src/main/java/com/itwill/hotdog/sql/ProductSQL.java
+++ src/main/java/com/itwill/hotdog/sql/ProductSQL.java
@@ -6,6 +6,10 @@ public class ProductSQL {
 	public final static String PRODUCT_SELECT_BY_NO=
 			"select * from product p join categories c on p.ct_no=c.ct_no where p_no=?";
 	public final static String PRODUCT_SELECT_BY_CNO=
-			"select * from categories c join product p on c.ct_no=p.ct_no where c.ct_no=?";
+			"select * from product where ct_no=?";
+	public final static String CATEGORIES_SELECT_All=
+			"select * from categories c join product p on c.ct_no=p.ct_no";
+	public final static String CATEGORY_SELECT_BY_CTNO=
+			"select * from categories c where ct_no=?";
 	
 }
