a166bed05b32318f5eb0afb6986686ab65c7f3dd
김민영 <117143516+ssssoooop@users.noreply.github.com>
2023-02-09 19:45:30 +0900

    Update ProductRepository.java

--- src/main/java/com/itwill/hotdog/repository/ProductRepository.java
+++ src/main/java/com/itwill/hotdog/repository/ProductRepository.java
@@ -1,11 +1,10 @@
-package com.itwill.hotdog.repository;
+ppackage com.itwill.hotdog.repository;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
-
 
 import javax.sql.DataSource;
 
@@ -21,36 +20,31 @@ PRODUCT 테이블에 제품 검색 등의 작업을한다.
 */
 public class ProductRepository {
 	private DataSource dataSource;
-	public ProductRepository() throws Exception{
 
-	  dataSource=DataSourceFactory.getDataSource();
+	public ProductRepository() throws Exception {
+
+		dataSource = DataSourceFactory.getDataSource();
 
 	}
+
 	/*
 	 * selelctByPK : 상품번호로 검색
 	 */
-	public Product findByPrimaryKey(int p_no) throws Exception{
-		Product product=null;
-		Connection con=null;
+	public Product findByPrimaryKey(int p_no) throws Exception {
+		Product product = null;
+		Connection con = null;
 		PreparedStatement pstmt = null;
-		ResultSet rs=null;
+		ResultSet rs = null;
 		try {
-		con=dataSource.getConnection();
-		pstmt=con.prepareStatement(ProductSQL.PRODUCT_SELECT_BY_NO);
-		pstmt.setInt(1, p_no);
-		rs=pstmt.executeQuery();
-		if (rs.next()) {
-			product=
-					new Product(
-							rs.getInt("p_no"),
-							rs.getString("p_name"), 
-							rs.getInt("p_price"), 
-							rs.getInt("p_discount"), 
-							rs.getString("p_desc"), 
-							rs.getString("p_img"), 
-							rs.getInt("p_click"),
-							null);
-		}
+			con = dataSource.getConnection();
+			pstmt = con.prepareStatement(ProductSQL.PRODUCT_SELECT_BY_NO);
+			pstmt.setInt(1, p_no);
+			rs = pstmt.executeQuery();
+			if (rs.next()) {
+				product = new Product(rs.getInt("p_no"), rs.getString("p_name"), rs.getInt("p_price"),
+						rs.getInt("p_discount"), rs.getString("p_desc"), rs.getString("p_img"), rs.getInt("p_click"),
+						null);
+			}
 		} finally {
 			if (con != null) {
 				con.close();
@@ -58,93 +52,121 @@ public class ProductRepository {
 		}
 		return product;
 	}
+
 	/*
 	 * selectAll : 상품전체검색
 	 */
-	public List<Product> findAll() throws Exception{
-		List<Product> productList=new ArrayList<Product>();
+	public List<Product> findAll() throws Exception {
+		List<Product> productList = new ArrayList<Product>();
 		Connection con = null;
 		PreparedStatement pstmt = null;
-		ResultSet rs= null;
-		
+		ResultSet rs = null;
+
 		try {
-			con=dataSource.getConnection();
-			pstmt=con.prepareStatement(ProductSQL.PRODUCT_SELECT_ALL);
-			rs=pstmt.executeQuery();
-			while(rs.next()) {
-				productList.add(new Product(
-							rs.getInt("p_no"),
-							rs.getString("p_name"), 
-							rs.getInt("p_price"), 
-							rs.getInt("p_discount"), 
-							rs.getString("p_desc"), 
-							rs.getString("p_img"), 
-							rs.getInt("p_click"),
-							null
-			));
+			con = dataSource.getConnection();
+			pstmt = con.prepareStatement(ProductSQL.PRODUCT_SELECT_ALL);
+			rs = pstmt.executeQuery();
+			while (rs.next()) {
+				productList.add(new Product(rs.getInt("p_no"), rs.getString("p_name"), rs.getInt("p_price"),
+						rs.getInt("p_discount"), rs.getString("p_desc"), rs.getString("p_img"), rs.getInt("p_click"),
+						new Categories(rs.getInt("ct_no"), rs.getString("ct_name"), rs.getString("ct_img"))));
 			}
-		}finally {
-		if (con != null) {
-		con.close();
-		}
-	}
-	return productList;
-
-}
-
-	public Categories findByCategoryNumber(int c_no) throws Exception{
-		Categories categories=null;
-		Connection con=null;
-		PreparedStatement pstmt = null;
-		ResultSet rs=null;
-		try {
-		con=dataSource.getConnection();
-		pstmt=con.prepareStatement(ProductSQL.PRODUCT_SELECT_BY_CNO);
-		pstmt.setInt(1, c_no);
-		rs=pstmt.executeQuery();
-		while(rs.next()) {
-			categories=new Categories(
-							rs.getInt("ct_no"),
-							rs.getString("ct_name"),
-							rs.getString("ct_img"),
-								new ArrayList<Product>()
-							);
-		}
-		}finally {
-			if(con!=null) {
+		} finally {
+			if (con != null) {
 				con.close();
 			}
 		}
-		
+		return productList;
+
+	}
+	public List<Product> findByCategoryNo(int ct_no) throws Exception {
+		List<Product> productList = new ArrayList<Product>();
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		try {
+			con = dataSource.getConnection();
+			pstmt = con.prepareStatement(ProductSQL.PRODUCT_SELECT_BY_CNO);
+			pstmt.setInt(1,ct_no);
+			rs = pstmt.executeQuery();
+			while (rs.next()) {
+				productList.add(new Product(rs.getInt("p_no"), rs.getString("p_name"), rs.getInt("p_price"),
+						rs.getInt("p_discount"), rs.getString("p_desc"), rs.getString("p_img"), rs.getInt("p_click"),
+						null));
+			}
+		} finally {
+			if (con != null) {
+				con.close();
+			}
+		}
+		return productList;
+
+	}
+	public Categories findByCategoryNumber(int c_no) throws Exception {
+		Categories categories = null;
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			con = dataSource.getConnection();
+			pstmt = con.prepareStatement(ProductSQL.PRODUCT_SELECT_BY_CNO);
+			pstmt.setInt(1, c_no);
+			rs = pstmt.executeQuery();
+			while (rs.next()) {
+				categories = new Categories(rs.getInt("ct_no"), rs.getString("ct_name"), rs.getString("ct_img"));
+			}
+		} finally {
+			if (con != null) {
+				con.close();
+			}
+		}
+
 		return categories;
 	}
 
+	public List<Categories> findAllCat() throws Exception {
+		List<Categories> categoriesList = new ArrayList<Categories>();
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
 
-public List<Categories> findAllCat() throws Exception{
-	List<Categories> categoriesList=new ArrayList<Categories>();
-	Connection con = null;
-	PreparedStatement pstmt = null;
-	ResultSet rs= null;
-	
-	try {
-		con=dataSource.getConnection();
-		pstmt=con.prepareStatement(ProductSQL.PRODUCT_SELECT_ALL);
-		rs=pstmt.executeQuery();
-		while(rs.next()) {
-			categoriesList.add(new Categories(
-						rs.getInt("ct_no"),
-						rs.getString("ct_name"),
-						rs.getString("ct_img"),
-						new ArrayList<Product>()
-		));
+		try {
+			con = dataSource.getConnection();
+			pstmt = con.prepareStatement(ProductSQL.PRODUCT_SELECT_ALL);
+			rs = pstmt.executeQuery();
+			while (rs.next()) {
+				categoriesList.add(new Categories(rs.getInt("ct_no"), rs.getString("ct_name"), rs.getString("ct_img")));
+			}
+		} finally {
+			if (con != null) {
+				con.close();
+			}
 		}
-	}finally {
-	if (con != null) {
-	con.close();
-	}
-}
-return categoriesList;
+		return categoriesList;
 
-}
+	}
+	public Categories findCategoryByCategoryNo(int ct_no) throws Exception {
+		Categories category = null;
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		try {
+			con = dataSource.getConnection();
+			pstmt = con.prepareStatement(ProductSQL.CATEGORY_SELECT_BY_CTNO);
+			pstmt.setInt(1, ct_no);
+			rs = pstmt.executeQuery();
+			while (rs.next()) {
+				category=new Categories(rs.getInt("ct_no"), rs.getString("ct_name"), rs.getString("ct_img"));
+			}
+		} finally {
+			if (con != null) {
+				con.close();
+			}
+		}
+		return category;
+
+	}
 
 }
