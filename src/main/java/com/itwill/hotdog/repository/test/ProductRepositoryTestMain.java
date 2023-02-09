a4b2028f4ba7effa95085c8d8bcef56926c152bc
김민영 <117143516+ssssoooop@users.noreply.github.com>
2023-02-09 10:30:23 +0900

    Update ProductRepositoryTestMain.java

--- src/main/java/com/itwill/hotdog/repository/test/ProductRepositoryTestMain.java
+++ src/main/java/com/itwill/hotdog/repository/test/ProductRepositoryTestMain.java
@@ -1,16 +1,20 @@
+
 package com.itwill.hotdog.repository.test;
 
 import com.itwill.hotdog.repository.ProductRepository;
 
 public class ProductRepositoryTestMain {
 
+	
 	public static void main(String[] args) throws Exception {
 		ProductRepository productRepository=new ProductRepository();
 		System.out.println("1.findByPrimaryKey");
-		System.out.println(productRepository.findByPrimaryKey(6));
 		System.out.println(productRepository.findByPrimaryKey(1));
 		System.out.println("2.findAll");
 		System.out.println(productRepository.findAll());
-	}
+		System.out.println("3.findByCategoryNumber");
+		System.out.println(productRepository.findByCategoryNumber(2));	
+		}
 
 }
+
