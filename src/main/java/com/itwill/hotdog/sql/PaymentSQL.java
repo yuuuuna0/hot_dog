95814ae489cf2ca7b66b807dfa433edda0b239fe
ssssoooop <ssssoooop@gmail.com>
2023-02-09 17:27:06 +0900

    ..

--- src/main/java/com/itwill/hotdog/sql/PaymentSQL.java
+++
@@ -1,7 +0,0 @@
-package com.itwill.hotdog.sql;
-
-public class PaymentSQL {
-	public final static String PAYMENT_SELECT_ALL="select * from payment";
-	public final static String PAYMENT_SELECT_BY_PM_NO="select * from payment where pm_no=?";
-
-}
