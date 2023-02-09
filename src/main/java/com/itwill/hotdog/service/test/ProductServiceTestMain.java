60a3ffc0b65a37a4b1fb3c27e8169d27b6e3ef2e
김민영 <117143516+ssssoooop@users.noreply.github.com>
2023-02-09 19:53:26 +0900

    Update ProductServiceTestMain.java

--- src/main/java/com/itwill/hotdog/service/test/ProductServiceTestMain.java
+++ src/main/java/com/itwill/hotdog/service/test/ProductServiceTestMain.java
@@ -8,9 +8,16 @@ public class ProductServiceTestMain {
 		ProductService productService=new ProductService();
     System.out.println("1.productList");
     System.out.println(productService.productList());
-    System.out.println("2.productDetail");
-    System.out.println(productService.productDetail(20));
-    System.out.println(productService.productDetail(2));
+    System.out.println("2.productListByCategoryNo");
+    System.out.println(productService.productListByCategoryNo(2));
+    System.out.println("3.productDetail");
+    System.out.println(productService.productDetail(3));
+    System.out.println("4.categoryDetail");
+    System.out.println(productService.categoriesDetail(1));
+    System.out.println("5.categoriesList");
+    System.out.println(productService.categoriesList());
+    System.out.println("6.findCategoryByCategoryNo");
+    System.out.println(productService.findCategoryByCategoryNo(1));
 	}
 
 }
