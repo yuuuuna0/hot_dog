ca6c8a5a1b0c0c12305b88a2e3a0669c7f068b36
김민영 <117143516+ssssoooop@users.noreply.github.com>
2023-02-09 10:10:17 +0900

    sql문 변경
    select from categories

--- src/main/java/com/itwill/hotdog/sql/ProductSQL.java
+++ src/main/java/com/itwill/hotdog/sql/ProductSQL.java
@@ -6,6 +6,6 @@ public class ProductSQL {
 	public final static String PRODUCT_SELECT_BY_NO=
 			"select * from product p join categories c on p.ct_no=c.ct_no where p_no=?";
 	public final static String PRODUCT_SELECT_BY_CNO=
-			"select * from product p join categories c on p.ct_no=c.ct_no where c.ct_no=?";
+			"select * from categories c join product p on c.ct_no=p.ct_no where c.ct_no=?";
 	
 }
